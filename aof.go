7aa40a557a809b2cf13133771953ded4df0c25e9
Edwin Ade <edwinmbonyjr@gmail.com>
2025-05-17 16:16:31 +0100

    feat: data persistence to local storage

--- aof.go
+++ aof.go
@@ -1,1 +1,78 @@
 package main
+
+import (
+	"bufio"
+	"io"
+	"os"
+	"sync"
+	"time"
+)
+
+type Aof struct {
+	file *os.File      // the file to be stored on disk
+	rd   *bufio.Reader // the reader of the file
+	mu   sync.Mutex
+}
+
+// returns a new AOF reader
+func NewAof(path string) (*Aof, error) {
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR, 0666)
+	if err != nil {
+		return nil, err
+	}
+	aof := &Aof{
+		file: f,
+		rd:   bufio.NewReader(f),
+	}
+	go func() {
+		for {
+			aof.mu.Lock()
+			err = aof.file.Sync()
+			if err != nil {
+				continue
+			}
+			aof.mu.Unlock()
+			time.Sleep(time.Second)
+		}
+	}()
+	return aof, nil
+}
+
+// closes the file when the server shuts down
+func (aof *Aof) Close() error {
+	aof.mu.Lock()
+	defer aof.mu.Unlock()
+	return aof.file.Close()
+}
+
+// AOF writer writes the command to the AOF file when the command is receieved
+func (aof *Aof) Write(value Value) error {
+	aof.mu.Lock()
+	defer aof.mu.Unlock()
+	_, err := aof.file.Write(value.Marshal())
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// AOF Reader reads all commands in the AOF file to restart our Redis server
+func (aof *Aof) Read(callback func(value Value)) error {
+	aof.mu.Lock()
+	defer aof.mu.Unlock()
+
+	resp := NewResp(aof.file)
+	for {
+		value, err := resp.Read()
+		if err == nil {
+			callback(value)
+		}
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
