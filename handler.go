8e930e722f0bd76f54ba66566d8702f861a505f4
Edwin Ade <edwinmbonyjr@gmail.com>
2025-05-17 16:17:07 +0100

    feat: add handler for hset hget and hgetall

--- handler.go
+++ handler.go
@@ -3,14 +3,19 @@ package main
 import "sync"
 
 var Handlers = map[string]func([]Value) Value{
-	"PING": ping,
-	"SET":  set,
-	"GET":  get,
+	"PING":    ping,
+	"SET":     set,
+	"HSET":    hset,
+	"GET":     get,
+	"HGET":    hget,
+	"HGETALL": hgetall,
 }
 
 var (
-	dict      = map[string]string{}
-	setsMutex = sync.RWMutex{}
+	setDict    = map[string]string{}
+	setsMutex  = sync.RWMutex{}
+	hsetDict   = map[string]map[string]string{}
+	hsetsMutex = sync.RWMutex{}
 )
 
 func ping(args []Value) Value {
@@ -21,26 +26,75 @@ func ping(args []Value) Value {
 }
 
 func set(args []Value) Value {
-	if len(args) < 2 {
+	if len(args) != 2 {
 		return Value{t: "error", str: "ERR wrong number of arguments for 'set' command"}
 	}
 	key := args[0].bulk
 	value := args[1].bulk
 	setsMutex.Lock()
-	dict[key] = value
+	setDict[key] = value
 	setsMutex.Unlock()
 	return Value{t: "string", str: "OK"}
 }
 
 func get(args []Value) Value {
-	if len(args) < 2 {
+	if len(args) != 1 {
 		return Value{t: "error", str: "ERR wrong number of arguments for 'GET' command"}
 	}
 	key := args[0].bulk
-	setsMutex.Lock()
-	value, ok := dict[key]
+	setsMutex.RLock()
+	value, ok := setDict[key]
 	if !ok {
 		return Value{t: "null"}
 	}
+	setsMutex.RUnlock()
 	return Value{t: "bulk", bulk: value}
+}
+
+func hset(args []Value) Value {
+	if len(args) != 3 {
+		return Value{t: "error", str: "ERR wrong number of arguments for 'HSET' command"}
+	}
+	hash := args[0].bulk
+	key := args[1].bulk
+	value := args[2].bulk
+	hsetsMutex.Lock()
+	if _, ok := hsetDict[hash]; !ok {
+		hsetDict[hash] = map[string]string{}
+	}
+	hsetDict[hash][key] = value
+	hsetsMutex.Unlock()
+	return Value{t: "string", str: "OK"}
+}
+
+func hget(args []Value) Value {
+	if len(args) != 2 {
+		return Value{t: "error", str: "ERR wrong number of arguments for 'HGET' command"}
+	}
+	hash := args[0].bulk
+	key := args[1].bulk
+	hsetsMutex.RLock()
+	val, ok := hsetDict[hash][key]
+	if !ok {
+		return Value{t: "null"}
+	}
+	hsetsMutex.RUnlock()
+	return Value{t: "bulk", bulk: val}
+}
+
+func hgetall(args []Value) Value {
+	if len(args) != 1 {
+		return Value{t: "error", str: "ERR wrong number of arguments for 'HGET' command"}
+	}
+	hash := args[0].bulk
+	hsetsMutex.RLock()
+	if _, ok := hsetDict[hash]; !ok {
+		return Value{t: "array", array: []Value{}}
+	}
+	values := make([]Value, 0)
+	for k, v := range hsetDict[hash] {
+		values = append(values, Value{t: "bulk", bulk: k}, Value{t: "bulk", bulk: v})
+	}
+	hsetsMutex.RUnlock()
+	return Value{t: "array", array: values}
 }
